0187b2fe46383849b44978602355f5fb3eaf4b17
gleonbs <gabriel.leonbs@gmail.com>
2020-05-11 19:59:05 -0300

    Atividade em desenvolvimento

---
+++ formularioUsuario.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<meta charset='utf-8'> 
+<title></title>
+</head>
+<body>
+    <form action="controleUsuario.php" method="post">
+      <label for="">Nome:</label>
+      <input type="text" name="nome"><br>
+      <label for="">Email:</label>
+      <input type="email" name="email"><br>
+      <label for="">Login:</label>
+      <input type="text" name="login"><br>
+      <label for="">Senha:</label>
+      <input type="password" name="senha"><br>
+      <input type="submit" value="cadastrar"><br>
+    </form>
+</body>
+</html>
